7e39f96719e0283e15cbe0900c2af11ddf215889
KID <50666207+KID421@users.noreply.github.com>
2019-11-20 10:24:48 +0800

    練習定義汽車類別與欄位

---
+++ UnityProject/Assets/Scripts/Car.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // 定義欄位 (宣告變數)
+    // 語法：
+    // 類型 名稱 結束
+    // 整數 int
+    int cc;
+    // 浮點數 float
+    float weight;
+    // 字串 string
+    string brand;
+    // 布林值 bool
+    bool _break;
+}
