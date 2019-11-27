02d6c6cb1411a903b064d1f4a719e406837aa843
KID <50666207+KID421@users.noreply.github.com>
2019-11-27 09:35:59 +0800

    練習定義玩家欄位：參數、道具與數值

--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -2,5 +2,17 @@
 
 public class Player : MonoBehaviour
 {
-    
+    [Header("動畫參數名稱")]
+    public string parRun = "跑步開關";
+    public string parJump = "跳躍開關";
+    public string parAtk = "攻擊觸發";
+    public string parDamage = "受傷觸發";
+    public string parDead = "死亡開關";
+    [Header("道具")]
+    public int countNeed = 10;
+    public int countCurrent;
+    [Header("數值")]
+    public bool isDead;
+    [Range(0, 500)]
+    public float hp = 100;
 }
